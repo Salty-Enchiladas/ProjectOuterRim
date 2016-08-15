44858e1b6009846312424ec38eab90f1278cd385
Borgeshc <hborges@digitaliris.com>
2016-08-14 22:07:07 -0500

    added warp drive

---
+++ Assets/StartWarp.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class StartWarp : MonoBehaviour 
+{
+    void Start()
+    {
+        StartCoroutine(Waiting());
+    }
+	IEnumerator Waiting()
+	{
+        yield return new WaitForSeconds(4);
+			GetComponent<Animator> ().SetBool ("Open", true);
+        yield return new WaitForSeconds(10);
+			StartWarping ();
+	}
+
+	void StartWarping()
+	{
+		Camera.main.fieldOfView++;
+        StartCoroutine(Ramping());
+    }
+
+    IEnumerator Ramping()
+    {
+        yield return new WaitForSeconds(.01f);
+        if (Camera.main.fieldOfView != 179)
+            StartWarping();
+        else if (Camera.main.fieldOfView == 179)
+        {
+            yield return new WaitForSeconds(3);
+            SceneManager.LoadScene("LoadingScene");
+        }
+    }
+}
