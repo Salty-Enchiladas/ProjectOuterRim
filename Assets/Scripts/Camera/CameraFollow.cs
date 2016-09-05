bd6165bde4c29fb60f47e1df5a7a2de18e0d0b3b
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-09-05 15:18:31 -0500

    Added an option for Camera Snap movement

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,6 +7,7 @@ public class CameraFollow : MonoBehaviour {
     GameObject playerTarget;
     public float cameraSpeed;
     public Vector3 cameraOffset;
+    public bool snapMovement;
 
 	// Use this for initialization
 	void Start () {
@@ -17,5 +18,28 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         transform.position = Vector3.Lerp(transform.position, new Vector3(playerTarget.transform.position.x + cameraOffset.x, playerTarget.transform.position.y + cameraOffset.y, playerTarget.transform.position.z + cameraOffset.z), Time.deltaTime * cameraSpeed);
-	}
+
+        if (snapMovement)
+        {
+
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            if (horizontal < -.5)
+            {
+                cameraOffset.x = 50;
+            }
+            if (horizontal > .5)
+            {
+                cameraOffset.x = -50;
+            }
+            if (vertical < -.5)
+            {
+                cameraOffset.y = 80;
+            }
+            if (vertical > .5)
+            {
+                cameraOffset.y = 0;
+            }
+        }
+    }
 }
