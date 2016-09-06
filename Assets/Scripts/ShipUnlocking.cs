5737633fa5f5898df173b70dac448eb7f673d2dc
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-09-05 19:02:55 -0500

    Added a visual to locked and unlocked ships

--- Assets/Scripts/ShipUnlocking.cs
+++ Assets/Scripts/ShipUnlocking.cs
@@ -6,11 +6,28 @@ public class ShipUnlocking : MonoBehaviour
 
     public string shipName;
     public bool unlocked;
+    public GameObject playButton;
+    public GameObject lockedButton;
 	void Start ()
     {
+        shipName = transform.name;
         if (PlayerPrefs.GetInt(shipName) == 1)
         {
             unlocked = true;
         }
 	}
+
+    void Update()
+    {
+        if (unlocked)
+        {
+            playButton.SetActive(true);
+            lockedButton.SetActive(false);
+        }
+        else if (!unlocked)
+        {
+            playButton.SetActive(false);
+            lockedButton.SetActive(true);
+        }
+    }
 }
