af8c35160ad49f31ffdcb36e8d0329dbe356e305
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-10-11 13:41:10 -0500

    Added speed increase over time for missile

--- Assets/Scripts/MissileMovement.cs
+++ Assets/Scripts/MissileMovement.cs
@@ -14,6 +14,7 @@ public class MissileMovement : MonoBehaviour {
     {
         nozzle = GameObject.Find("MissileNozzle");
         target = nozzle.GetComponent<FireMissile>().target;
+        StartCoroutine(IncreaseSpeed());
     }
 
     // Update is called once per frame
@@ -21,5 +22,17 @@ public class MissileMovement : MonoBehaviour {
     {
         transform.LookAt(target.transform);
         transform.Translate(Vector3.forward * missileSpeed * Time.deltaTime);
+    }
+
+    IEnumerator IncreaseSpeed()
+    {
+        missileSpeed = missileSpeed * 1.5f;
+        yield return new WaitForSeconds(1);
+        missileSpeed = missileSpeed * 1.5f;
+        yield return new WaitForSeconds(1);
+        missileSpeed = missileSpeed * 1.5f;
+        yield return new WaitForSeconds(.5f);
+        missileSpeed = missileSpeed * 1.5f;
+        yield return new WaitForSeconds(.5f);
     }
 }
